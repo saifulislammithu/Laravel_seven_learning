5dcb8abe4b3fcf4cb380544eeafbf78d1c7d8272
saifulislammithu <saifulrubd@gmail.com>
2020-07-08 00:02:39 +0600

    Laravel/ui_commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home','UserController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
