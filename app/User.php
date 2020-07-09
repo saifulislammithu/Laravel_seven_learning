f9e881010538ce363922234a308d1513908fe74f
saifulislammithu <saifulrubd@gmail.com>
2020-07-10 01:05:36 +0600

    Adding staic method to upload and stote profile image to User modec class

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 
 class User extends Authenticatable
 {
@@ -16,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','profile_image',
     ];
 
     /**
@@ -36,4 +37,22 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public static function upload_profile_image($image)
+    {
+        $fileName=$image->getClientOriginalname();
+        (new self())->deleteOldImage();
+        $image->storeAs('images',$fileName,'public');
+        auth()->user()->update(['profile_image'=>$fileName]);
+    }
+    protected function deleteOldImage()
+    {
+        if($this->profile_image)
+            {
+                Storage::delete('/public/images/'.$this->profile_image);
+                
+    }
+    }
+
+    
 }
+
